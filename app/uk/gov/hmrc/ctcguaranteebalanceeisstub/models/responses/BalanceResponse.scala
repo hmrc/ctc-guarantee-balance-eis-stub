b7d0a77595c7c1818cfd323ae3651be0a34e72d3
liandra-giacomine <liandra.giacomine@opencastsoftware.com>
2023-01-11 13:42:42 +0000

    Added POST request for getting the balance

--- app/uk/gov/hmrc/ctcguaranteebalanceeisstub/models/responses/BalanceResponse.scala
+++ app/uk/gov/hmrc/ctcguaranteebalanceeisstub/models/responses/BalanceResponse.scala
@@ -1,3 +1,32 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ */
+
 package uk.gov.hmrc.ctcguaranteebalanceeisstub.models.responses
 
-class BalanceResponse {}
+import play.api.libs.functional.syntax.toFunctionalBuilderOps
+import play.api.libs.json.Format
+import play.api.libs.json.Json
+import play.api.libs.json.__
+import uk.gov.hmrc.ctcguaranteebalanceeisstub.models.AccessCode
+import uk.gov.hmrc.ctcguaranteebalanceeisstub.models.GuaranteeReferenceNumber
+
+import scala.util.Random
+
+case class BalanceResponse(guaranteeReferenceNumber: GuaranteeReferenceNumber, remainingBalance: Balance)
+
+case class Balance(value: Int) extends AnyVal
+
+object Balance {
+  implicit val balanceFormat = Json.valueFormat[Balance]
+
+  def generateBalance(): Balance = Balance(Random.nextInt(10000))
+}
+
+object BalanceResponse {
+
+  implicit val format: Format[BalanceResponse] =
+    ((__ \ "GRN").format[GuaranteeReferenceNumber] and (__ \ "remainingBalance")
+      .format[Balance])(BalanceResponse.apply(_, _), response => (response.guaranteeReferenceNumber, response.remainingBalance))
+}
--- test/uk/gov/hmrc/ctcguaranteebalanceeisstub/controllers/models/BalanceResponseSpec.scala
+++ test/uk/gov/hmrc/ctcguaranteebalanceeisstub/controllers/models/BalanceResponseSpec.scala
@@ -1,3 +1,27 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ */
+
 package uk.gov.hmrc.ctcguaranteebalanceeisstub.controllers.models
 
-class BalanceRequestSpec {}
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.should.Matchers
+import play.api.libs.json.Json
+import uk.gov.hmrc.ctcguaranteebalanceeisstub.controllers.Generators
+import uk.gov.hmrc.ctcguaranteebalanceeisstub.models.responses.BalanceResponse
+
+class BalanceResponseSpec extends AnyFreeSpec with Matchers with Generators {
+
+  for (balanceResponse <- balanceResponseGenerator().sample) {
+    val expectedJson = Json.obj("GRN" -> balanceResponse.guaranteeReferenceNumber, "remainingBalance" -> balanceResponse.remainingBalance.value)
+
+    "AccessCodeResponse should serialize as expected" in {
+      expectedJson.validate[BalanceResponse].get shouldBe balanceResponse
+    }
+
+    "AccessCodeResponse should deserialize as expected" in {
+      Json.toJson(balanceResponse) shouldBe expectedJson
+    }
+  }
+}
